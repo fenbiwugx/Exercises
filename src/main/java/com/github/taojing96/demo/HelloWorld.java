1c0aaedbf14801a106a7c3b08a5bbc7d4768f98e
Wu Guangxu <wugx@fenbi.com>
2019-10-21 23:11:34 +0800

    ADD: Java foundation outline

---
+++ src/main/java/com/github/taojing96/demo/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)HelloWorld.java, Oct 21, 2019.
+ * <p>
+ * Copyright 2019 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.github.taojing96.demo;
+
+/**
+ * @author wugx
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello taojing96!");
+    }
+
+}
